76f607b11a239216be27ce3398078c44992f3b89
Diego García <djgg91@gmail.com>
2016-02-04 14:49:19 +0100

    Initial commit

---
+++ src/main/scala/org/apache/spark/mllib/tree/PCARD.scala
@@ -0,0 +1,106 @@
+package org.apache.spark.mllib.tree
+
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.mllib.feature.{PCA, PCAModel}
+import org.apache.spark.rdd._
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.linalg._
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.classification._
+import scala.util.Random
+
+class PCARD private (val data: RDD[LabeledPoint], val nTrees: Int, val cuts: Int) extends Serializable{
+
+  private val disc: Array[Array[Array[Double]]] = new Array[Array[Array[Double]]](nTrees)
+  private val pcaList: Array[PCAModel] = new Array[PCAModel](nTrees)
+  private val models: Array[PipelineModel] = new Array[PipelineModel](nTrees)
+  private var labelsInd: Array[String] = new Array[String](1)
+
+  private def assignDiscreteValue(value: Double, thresholds: Seq[Double]) = {
+    if(thresholds.isEmpty){
+      value
+    }else{
+      val ret = thresholds.indexWhere{value <= _}
+      if(ret == -1){
+        thresholds.size.toDouble
+      }else{
+        ret.toDouble
+      }
+    }
+  }
+
+  def runTrain(): PCARDModel ={
+
+    val sqlContext= new org.apache.spark.sql.SQLContext(data.context)
+    import sqlContext.implicits._
+
+    for(c <- 0 until nTrees){
+
+      //RD
+      val cortes = Seq.fill(cuts-1)(Random.nextInt(data.count.toInt))
+      val indexKey = data.zipWithIndex.map{case (k,v) => (v,k)}
+      val temp  = new Array[Array[Double]](cortes.size)
+
+      for(i <- cortes.indices){
+        temp(i) = indexKey.lookup(cortes(i)).head.features.toArray
+      }
+
+      val trans = temp.transpose.zipWithIndex.map{case(l,i) =>
+        val feat = l.sorted.distinct
+        if (feat.length == 1){
+          val col = data.map(l=>l.features.toArray.slice(i,i+1)).collect.flatten.distinct
+          (util.Random.shuffle(col.toList) take cuts-1).sorted.toArray
+        }else{
+          feat
+        }
+      }
+
+      disc(c) = trans
+
+      val discData = data.map{ l =>
+        val features = l.features.toArray
+        val newValues = for (i <- features.indices)
+          yield assignDiscreteValue(features(i), trans(i).toSeq)
+        LabeledPoint(l.label, Vectors.dense(newValues.toArray))
+      }
+
+      //PCA
+
+      val tam = data.first().features.size
+
+      val rnd = new scala.util.Random
+      val range = 1 to tam-1
+      val size = range(rnd.nextInt(range.length))
+
+      val pca = new PCA(size).fit(data.map(_.features))
+      pcaList(c) = pca
+      val pcaData = data.map(p => p.copy(features = pca.transform(p.features)))
+
+      //PCARD
+      val PCARD = pcaData.zip(discData).map(l => LabeledPoint(l._1.label, Vectors.dense(l._2.features.toArray ++ l._1.features.toArray))).toDF()
+
+      val labelIndexer = new StringIndexer()
+        .setInputCol("label")
+        .setOutputCol("indexedLabel")
+        .fit(PCARD)
+
+      labelsInd = labelIndexer.labels
+
+      val dt = new DecisionTreeClassifier()
+        .setLabelCol("indexedLabel")
+        .setFeaturesCol("features")
+
+      val pipeline = new Pipeline()
+        .setStages(Array(labelIndexer, dt))
+
+      models(c) = pipeline.fit(PCARD)
+    }
+    new PCARDModel(nTrees, disc, pcaList, models, labelsInd)
+  }
+}
+
+object PCARD{
+  def train(input: RDD[LabeledPoint], nTrees: Int, cuts: Int) ={
+    new PCARD(input, nTrees, cuts).runTrain()
+  }
+}
---
+++ src/main/scala/org/apache/spark/mllib/tree/PCARDModel.scala
@@ -0,0 +1,63 @@
+package org.apache.spark.mllib.tree
+
+import org.apache.spark.ml.PipelineModel
+import org.apache.spark.mllib.feature.PCAModel
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.Row
+
+class PCARDModel(val nTrees: Int, val disc: Array[Array[Array[Double]]], val pcaList: Array[PCAModel], val models: Array[PipelineModel], val labelsInd: Array[String]) extends Serializable{
+
+  private def assignDiscreteValue(value: Double, thresholds: Seq[Double]) = {
+    if(thresholds.isEmpty){
+      value
+    }else{
+      val ret = thresholds.indexWhere{value <= _}
+      if(ret == -1){
+        thresholds.size.toDouble
+      }else{
+        ret.toDouble
+      }
+    }
+  }
+
+  def test(test: RDD[LabeledPoint]): Array[Double] ={
+
+    val sqlContext= new org.apache.spark.sql.SQLContext(test.context)
+    import sqlContext.implicits._
+
+    val tama = test.count.toInt
+    val totalPredictions = Array.fill(tama)(Array.fill(labelsInd.size)(0.0))
+    val dataTest = test.map(_.features)
+
+    for(c <- 0 until nTrees){
+
+      //RD
+      val discTest = dataTest.map{ l =>
+        val features = l.toArray
+        val newValues = for (i <- features.indices)
+          yield assignDiscreteValue(features(i), disc(c)(i).toSeq)
+        Vectors.dense(newValues.toArray)
+      }
+      val featDisc = discTest.map(l => l.toArray)
+
+      //PCA
+
+      val pcaData = test.map(p => LabeledPoint(p.label, pcaList(c).transform(p.features)))
+
+      //PCARD
+
+      val PCARD = pcaData.zip(featDisc).map(l => LabeledPoint(l._1.label, Vectors.dense(l._2 ++ l._1.features.toArray))).toDF()
+
+      val predictions = models(c).transform(PCARD).select("probability").collect()
+      var i = 0
+      predictions.foreach { case Row(prob: Vector) =>
+        totalPredictions(i) = Array(totalPredictions(i), prob.toArray).transpose.map(_.sum)
+        i += 1
+      }
+    }
+    val pos = totalPredictions.zipWithIndex.map{case (k,v) => k.indexOf(k.max).toDouble}
+    pos.map(l => labelsInd(l.toInt).toDouble)
+  }
+}
